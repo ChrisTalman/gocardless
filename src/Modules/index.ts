00a99665c7f9560c0acd6cc4c042afb4018bd912
Chris Talman <cewe.mail@gmail.com>
2019-10-18 21:25:30 +0100

    Removed debug code.

--- src/Modules/index.ts
+++ src/Modules/index.ts
@@ -6,11 +6,5 @@ export interface Metadata
 	[key: string]: string;
 };
 
-// DEBUG
-// import { Client } from './Client';
-// const gocardless = new Client({baseSubdomain: 'api-sandbox', accessToken: '123', version: 'abc'});
-// gocardless.payments.create({amount: 123, currency: 'abc'});
-// DEBUG
-
 // Exports
 export { Client } from './Client';
